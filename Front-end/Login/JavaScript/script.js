bb24d4d8dc7f811dcb37ab265e9a883e382c156d
UlfGunnar <ulfgunnar6@gmail.com>
2026-04-25 17:07:58 -0300

    Delete Front-end directory

--- Front-end/Login/JavaScript/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('exibir_senha').addEventListener('change', function() {
-    const campo = document.getElementById('senha');
-    campo.type = this.checked ? 'text' : 'password';
-})
