e963eb260de0ee88c74f9e33067b188824e44651
Pedro Lucas Rodrigues <pedrolr2060@gmail.com>
2026-05-06 21:24:03 -0300

    DDL

--- Banco_de_dados/Database/DDL.sql
+++ Banco_de_dados/Database/DDL.sql
@@ -3,11 +3,11 @@ default character set utf8
 default collate utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS cliente (
-	
+    
     id_cpf CHAR(11) NOT NULL PRIMARY KEY,
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(80)NOT NULL,
-    num_celular CHAR(11) NOT NULL,
+    num_celular CHAR(11),
     bairro VARCHAR(20) NOT NULL,
     rua CHAR(50) NOT NULL,
 	num_residencia CHAR(10) NOT NULL,
@@ -15,33 +15,31 @@ CREATE TABLE IF NOT EXISTS cliente (
 )
 default charset = utf8;
 
-CREATE TABLE IF NOT EXISTS animal (
+CREATE TABLE IF NOT EXISTS animal (  
 
 	id_animal INT AUTO_INCREMENT PRIMARY KEY,
     fk_cpf CHAR(11) NOT NULL,
     nome_animal VARCHAR(30) NOT NULL,
-    genero_animal CHAR(1),
+    genero_animal CHAR(1) NOT NULL,
     especie VARCHAR(15) NOT NULL,
-    raca VARCHAR(15)NOT NULL,
-	peso_animal DECIMAL(5,2),
+    raca VARCHAR(15),
+	peso_animal DECIMAL(5,2) NOT NULL,
     
-    FOREIGN KEY (fk_cpf) REFERENCES cliente(id_cpf)
-    
+    FOREIGN KEY (fk_cpf) REFERENCES cliente(id_cpf)   
 ) 
 default charset = utf8;
 
 CREATE TABLE IF NOT EXISTS medico ( 
 	
-    matricula_medico INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    matricula_medico INT AUTO_INCREMENT PRIMARY KEY,
     nome_medico VARCHAR(60) NOT NULL,
-    num_celular CHAR(11) NOT NULL,
+    num_celular CHAR(11),
     email VARCHAR(80) NOT NULL,
     bairro VARCHAR(20) NOT NULL,
     rua CHAR(50) NOT NULL,
 	num_residencia CHAR(10) NOT NULL,
     complemento VARCHAR(50),
-    dt_nascimento DATE NOT NULL
-    
+    dt_nascimento DATE NOT NULL 
 )
 default charset = utf8;
 
@@ -56,8 +54,6 @@ CREATE TABLE IF NOT EXISTS secretaria (
 	num_residencia CHAR(10) NOT NULL,
     complemento VARCHAR(50),
     dt_nascimento DATE NOT NULL
-    
-
 )
 default charset = utf8;
 
@@ -66,23 +62,58 @@ CREATE TABLE IF NOT EXISTS tipo_consulta (
     id_tipo INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	finalidade VARCHAR(20),
     valor DECIMAL(7,2)
-    
 )
 default charset = utf8;
 
 CREATE TABLE IF NOT EXISTS consulta (
 	
-	id_consulta int AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	id_consulta INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     fk_matricula_medico INT NOT NULL,
     fk_animal INT NOT NULL,
     fk_cpf CHAR(11) NOT NULL,
 	fk_finalidade VARCHAR(20),
     dt_consulta DATE NOT NULL,
-    hora_consulta TIME NOT NULL,
+    hr_consulta TIME NOT NULL,
 		
     FOREIGN KEY (fk_matricula_medico) REFERENCES medico(matricula_medico),
 	FOREIGN KEY (fk_animal) REFERENCES animal(id_animal),
     FOREIGN KEY (fk_cpf) REFERENCES	cliente(id_cpf),
     FOREIGN KEY (fk_finalidade) REFERENCES tipo_consulta(finalidade)
+)
+default charset = utf8;
+
+CREATE TABLE IF NOT EXISTS login (
+
+    id_conta INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    fk_cpf CHAR(11) NOT NULL,
+    fk_matricula_secretaria INT NOT NULL,
+    fk_matricula_medico INT NOT NULL,
+    usuario VARCHAR(20) NOT NULL,
+    senha VARCHAR(10) NOT NULL,
+
+    FOREIGN KEY (fk_cpf) REFERENCES	cliente(id_cpf),
+    FOREIGN KEY (fk_matricula_secretaria) REFERENCES secretaria(matricula_secretaria),
+    FOREIGN KEY (fk_matricula_medico) REFERENCES medico(matricula_medico)
+)
+default charset = utf8;
+
+CREATE TABLE IF NOT EXISTS histotico (
+
+    id_historico INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    fk_animal INT NOT NULL,
+    fk_tipo INT NOT NULL,
+    fk_cpf INT NOT NULL,
+    fk_consulta INT NOT NULL,
+    remedio VARCHAR(40) NOT NULL,
+    diagnostico VARCHAR(40) NOT NULL,
+    fk_dt_consulta DATE NOT NULL,
+    fk_hr_consulta TIME NOT NULL,
+
+    FOREIGN KEY (fk_animal) REFERENCES animal(id_animal),
+    FOREIGN KEY (fk_tipo) REFERENCES tipo_consulta(id_tipo),
+    FOREIGN KEY (fk_cpf) REFERENCES	cliente(id_cpf),
+    FOREIGN KEY (fk_consulta) REFERENCES consulta(id_consulta),
+    FOREIGN KEY (fk_dt_consulta) REFERENCES consulta(dt_consulta),
+    FOREIGN KEY (fk_hr_consulta) REFERENCES consulta(hr_consulta)
 )
 default charset = utf8;
