124ec285c1eef762f93d60b664eaf6d956b4cd5c
pedrolucasrodri <pedrolr2060@gmail.com>
2026-05-26 21:53:50 -0300

    SQL

--- Banco_de_dados/Database/DDL.sql
+++ Banco_de_dados/Database/DDL.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS animal (
     raca VARCHAR(15),
 	peso DECIMAL(5,2) NOT NULL,
     
-    FOREIGN KEY (fk_cpf) REFERENCES cliente(id_cpf)   
+    FOREIGN KEY (fk_cpf) REFERENCES cliente(cpf)   
 ) 
 default charset = utf8;
 
@@ -77,7 +77,7 @@ CREATE TABLE IF NOT EXISTS consulta (
 		
     FOREIGN KEY (fk_matricula_medico) REFERENCES medico(matricula_medico),
 	FOREIGN KEY (fk_animal) REFERENCES animal(id_animal),
-    FOREIGN KEY (fk_cpf) REFERENCES	cliente(id_cpf),
+    FOREIGN KEY (fk_cpf) REFERENCES	cliente(cpf),
     FOREIGN KEY (fk_tipo_consulta) REFERENCES tipo_consulta(id_tipo)
 )
 default charset = utf8;
@@ -90,7 +90,7 @@ CREATE TABLE IF NOT EXISTS login (
     fk_matricula_medico INT NOT NULL,
     senha VARCHAR(50) NOT NULL,
 
-    FOREIGN KEY (fk_cpf) REFERENCES	cliente(id_cpf),
+    FOREIGN KEY (fk_cpf) REFERENCES	cliente(cpf),
     FOREIGN KEY (fk_matricula_secretaria) REFERENCES secretaria(matricula_secretaria),
     FOREIGN KEY (fk_matricula_medico) REFERENCES medico(matricula_medico)
 )
@@ -108,7 +108,7 @@ CREATE TABLE IF NOT EXISTS historico (
 
     FOREIGN KEY (fk_animal) REFERENCES animal(id_animal),
     FOREIGN KEY (fk_tipo) REFERENCES tipo_consulta(id_tipo),
-    FOREIGN KEY (fk_cpf) REFERENCES	cliente(id_cpf),
+    FOREIGN KEY (fk_cpf) REFERENCES	cliente(cpf),
     FOREIGN KEY (fk_consulta) REFERENCES consulta(id_consulta)
     
 )
