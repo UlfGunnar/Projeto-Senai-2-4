b8108bca90efe6a51eda9ac9075de920a82f2deb
IgorFlores123 <igorfloresbr@gmail.com>
2026-05-28 19:56:48 -0300

    alterações na ordem dos atributos

--- DAO/classes.py
+++ DAO/classes.py
@@ -1,13 +1,13 @@
-class Paciente():
+class Cliente():
     def __init__(self, cpf: str,nome_cliente: str, email: str, numero: int, bairro: str, rua: str, celular: str, complemento: str ):
-        self.cpf = cpf
         self.nome_cliente = nome_cliente
+        self.cpf = cpf
+        self.celular = celular
         self.email = email
-        self.numero = numero
         self.bairro = bairro
         self.rua = rua
-        self.celular = celular
         self.complemento = complemento
+        self.numero = numero
 
 class Animal():
     def __init__(self, id_animal: int, fk_cpf: str, nome_animal: str, raca: str, especie: str, genero: str, peso: float):
@@ -24,31 +24,31 @@ class Medico():
     def __init__(self, matricula_medico: int, nome_medico: str, num_celular: str, rua: str, complemento: str, dt_nascimento: str, email: str, bairro: str):
         self.matricula_medico = matricula_medico
         self.nome_medico = nome_medico
-        self.num_celular = num_celular
-        self.rua = rua
-        self.complemento = complemento
         self.dt_nascimento = dt_nascimento
+        self.num_celular = num_celular
         self.email = email
         self.bairro = bairro
+        self.rua = rua
+        self.complemento = complemento
 
 class Secretaria():
     def __init__(self, matricula_secretaria: int, nome_secretaria: str, dt_nascimento: str, email: str, rua: str, num_celular: str, complemento: str, bairro: str):
-        self.matricula_secretaria = matricula_secretaria
-        self.nome_secretaria = nome_secretaria
+        self.matricula_medico = matricula_secretaria
+        self.nome_medico = nome_secretaria
         self.dt_nascimento = dt_nascimento
-        self.email = email
-        self.rua = rua
         self.num_celular = num_celular
-        self.complemento = complemento
+        self.email = email
         self.bairro = bairro
+        self.rua = rua
+        self.complemento = complemento
 class Consulta():
     #data = date
     #hora = date
     def __init__(self, id_consulta: int, fk_matricula_medico: int, fk_animal: int, fk_cpf: str, fk_tipo_consulta: int, dt_consulta, hr_consulta):
         self.id_consulta = id_consulta
-        self.fk_matricula_medico = fk_matricula_medico
         self.fk_animal = fk_animal
         self.fk_cpf = fk_cpf
+        self.fk_matricula_medico = fk_matricula_medico
         self.fk_tipo_consulta = fk_tipo_consulta
         self.dt_consulta = dt_consulta
         self.hr_consulta = hr_consulta
@@ -75,11 +75,11 @@ class Historico():
     def __init__(self, id_historico: int, fk_animal: int, fk_tipo: int, fk_cpf: str, fk_consulta: int, remedio: str, diagnostico: str, data, hora):
         self.id_historico = id_historico
         self.fk_animal = fk_animal
-        self.fk_tipo = fk_tipo
         self.fk_cpf = fk_cpf
+        self.fk_tipo = fk_tipo
         self.fk_consulta = fk_consulta
-        self.remedio = remedio
         self.diagnostico = diagnostico
+        self.remedio = remedio
         self.data = data
         self.hora = hora
 
