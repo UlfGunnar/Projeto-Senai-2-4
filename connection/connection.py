9ad2b9e32030cf1f94c74d622fad4a70c11018e6
Pedro Lucas Rodrigues <pedrolr2060@gmail.com>
2026-05-28 19:58:15 -0300

    Conexão

--- connection/connection.py
+++ connection/connection.py
@@ -2,17 +2,6 @@ import mysql.connector
 
 def get_connection():
     try:
-
-        return mysql.connector.connect(
-            host='localhost',
-            user='root',        
-            password='dogtor2026',
-            database='db_dogtor',
-            port=3306
-        )
-    except mysql.connector.Error as err:
-        print(f"Erro ao connectar ao MySQL: {err}")
-        return None
         conn = mysql.connector.connect(
             host="localhost",
             user="root",
