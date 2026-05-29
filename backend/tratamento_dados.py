4e367436b52251b58ada07cc99d9eccb32207483
IgorFlores123 <igorfloresbr@gmail.com>
2026-05-28 22:07:09 -0300

    tratamento de dados

--- backend/tratamento_dados.py
+++ backend/tratamento_dados.py
@@ -5,29 +5,34 @@ import re
 6 caracteres minimos
 1 caracter especial
 """""
-def validar_senha(self, senha):
+def validar_senha(self, senha): #
     self.senha = senha
-    senha_especial = any(not c.isalnum() for c in senha)
-    if senha_especial and len(senha) >= 6:
+    senha_sem_espaço = senha.replace(" ", "")
+    senha_especial = any(not c.isalnum() for c in senha_sem_espaço)
+    if senha_especial and len(senha_sem_espaço) >= 6:
         return True
     else:
         return False
     
-def validar_nome(self, nome):
-    self.nome = nome
-    if nome.replace(" ", "").isalpha():
-        return True
-    else:
-        return False
+class validar_nome: #validado, vai devolver o nome sem espaços (corretamente) "igor flores"
+    def validar_nome(self, nome):
+        self.nome = nome.strip()
+        if self.nome.replace(" ", "").isalpha():
+            return True
+        else:
+            return False
     
-def validar_celular(self,numero, numero_tratado):
+def validar_celular(self, numero, numero_tratado):
     self.numero = numero
-    self.numero_tratado = numero_tratado
+    self.numero_tratado
     try:
         numero_tratado = re.sub(r"\D", "", numero)
-    except:
-        #inserir aqui alguma coisa
-    if len(numero_tratado) == 11:
+    except (NameError, TypeError):
         return False
-    else:
+    if len(numero_tratado) == 11:
         return True
+    else:
+        return False
+
+def validar_email(self, email):
+    self.email = email
