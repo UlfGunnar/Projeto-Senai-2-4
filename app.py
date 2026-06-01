087cc6e3107e14ac76c98f4f5aa5f8a63b7f9de6
UlfGunnar <ulfgunnar6@gmail.com>
2026-06-01 18:24:49 -0300

    Processamento de dados da tela de registro de funcionários

--- app.py
+++ app.py
@@ -49,6 +49,20 @@ def processar_register():
 def register_func_page():
     return render_template('Register_funcionario.html')
 
+@app.route('/register_func', methods=['POST'])
+def processar_register_func():
+    nome        = request.form.get('nome_register')
+    cpf         = request.form.get('cpf_register')
+    senha       = request.form.get('senha_register')
+    data_nasc   = request.form.get('nasc_register')
+
+    celular     = request.form.get('celular_register')
+    email       = request.form.get('email_register')
+
+    bairro      = request.form.get('bairro_register')
+    rua         = request.form.get('rua_register')
+    complemento = request.form.get('complemento_register')
+
 #------------------------------#
 #---------MENU CLIENTE---------#
 #------------------------------#
