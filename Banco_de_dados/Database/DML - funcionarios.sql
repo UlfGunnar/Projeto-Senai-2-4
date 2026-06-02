1afb4dc7ce690900106b14ab249e32d6e94787f7
Pedro Lucas Rodrigues <pedrolr2060@gmail.com>
2026-06-02 20:28:14 -0300

    Atualizado

---
+++ Banco_de_dados/Database/DML - funcionarios.sql
@@ -0,0 +1,61 @@
+USE db_dogtor;
+
+INSERT INTO funcionarios (
+    nome_funcionario, 
+    num_celular, 
+    email, 
+    bairro, 
+    rua, 
+    complemento, 
+    dt_nasciment
+)
+
+VALUES (
+    'Albert Igor',
+    '47999999999',
+    'albertigor_dogtor@gmail.com.br',
+    'Ativadores',
+    'Columbia de baixo',
+    'Atras do bosque 2',
+    '1990-05-06'
+);
+
+INSERT INTO funcionarios (
+    nome_funcionario, 
+    num_celular, 
+    email, 
+    bairro, 
+    rua, 
+    complemento, 
+    dt_nasciment
+)
+
+VALUES (
+    'Gunnardson Alves',
+    '47999999990',
+    'gunardson_dogtor@gmail.com.br',
+    'Indigena',
+    'Rua dos colnos',
+    'Oca Nº5',
+    '1976-12-07'
+)
+
+INSERT INTO funcionarios (
+    nome_funcionario, 
+    num_celular, 
+    email, 
+    bairro, 
+    rua, 
+    complemento, 
+    dt_nasciment
+)
+
+VALUES (
+    'Lucroqueta Gebson',
+    '47999999990',
+    'lucroqueta_dogtorSecretaria@gmail.com.br',
+    'Guarana',
+    'Rua do gas',
+    'Pavilhão 1',
+    '2004-11-17'
+)
