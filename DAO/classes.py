34dac0ebed2c643e9cd8033762b8e25e561e83bf
Igor Flores <igorfloresbr@gmail.com>
2026-06-06 19:23:59 -0300

    alteração de nome_func pra nome_funcionario

--- DAO/classes.py
+++ DAO/classes.py
@@ -21,10 +21,10 @@ class Animal():
         
 class funcionario():
     #data_nasc = date
-    def __init__(self, id_funcionario: int, fk_id_cargo: int, nome_func: str, num_cel: str, email: str, bairro: str, rua: str, complemento: str, data_nasc):
+    def __init__(self, id_funcionario: int, fk_id_cargo: int, nome_funcionario: str, num_cel: str, email: str, bairro: str, rua: str, complemento: str, data_nasc):
         self.id_funcionario = id_funcionario
         self.fk_id_cargo = fk_id_cargo
-        self.nome_func = nome_func
+        self.nome_funcionario = nome_funcionario
         self.num_cel = num_cel
         self.email = email
         self.bairro = bairro
