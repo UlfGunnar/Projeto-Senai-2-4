9c92009be18b03bc22aad0ac1b41b80dbc7b2b07
UlfGunnar <ulfgunnar6@gmail.com>
2026-06-08 21:54:19 -0300

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from DAO.Cliente_dao import ClienteDAO
 from DAO.Funcionarios_dao import FuncionarioDAO
 from DAO.Animal_dao import AnimalDAO
 from DAO.Consulta_dao import ConsultaDAO
-from re import re
+import re
 
 app = Flask(__name__)
 app.secret_key = 'chave_secreta'
@@ -50,10 +50,6 @@ def processar_register():
     
     cpf = cpf.strip().replace(".", "").replace("-", "")
     celular = re.sub(r"\D", "", celular)
-
-
-    if not nome or not cpf or not senha or not celular or not email:
-        return redirect(url_for('register_page'))
 
     cpf_valido     = tratar_dados.validar_cpf(cpf)
     senha_valida   = tratar_dados.validar_senha(senha)
@@ -150,12 +146,16 @@ def processar_register_func():
         flash("CPF inválido", "error")
     if senha_valida == False:
         flash("Senha inválida, mínimo de 6 digitos e pelo menos 1 caracter especial", "error")
-    if data_nasc_valida == False:
-        flash("Data inválido", "error")
     if celular_valido == False:
         flash("celular inválido", "error")
     if email_valido == False:
         flash("email inválido", "error")
+    if bairro_valido == False:
+        flash("Bairro inválido", "error")
+    if rua_valida == False:
+        flash("Rua inválido", "error")
+    if cargo_valido == False:
+        flash("Selecione um cargo")
     
     
     if (nome_valido and cpf_valido and senha_valida and data_nasc_valida and 
@@ -267,7 +267,7 @@ def processar_marcar_consulta():
     proprio_end     = request.form.get('endereco')
 
     tipo_valido = bool(tipo_consulta)
-    nome_valido = tratar_dados.validar_nome.validar_nome(nome_animal)
+    nome_valido = tratar_dados.validar_nome(nome_animal)
     data_valida = tratar_dados.validar_data(data_consulta)
     hora_valida = tratar_dados.validar_horario(hora_consulta)
 
@@ -293,7 +293,7 @@ def processar_marcar_consulta():
         else:
             complemento_valido = True
             
-    if (tipo_valido and especie_valida and nome_valido and data_valida and 
+    if (tipo_valido and especie_valida and nome_valido and data_valida and  
         hora_valida and bairro_valido and rua_valida and numero_valido and complemento_valido):
         
         cpf_cliente = "12345678901" #Provisório
@@ -325,8 +325,36 @@ def processar_marcar_consulta():
         dao = ClienteDAO()
         dao.salvar_dados(nova_consulta)
         
+        flash('Consulta marcarda!', 'sucess')
         return redirect(url_for('menu_cliente_page'))
     
+    if tipo_valido == False:
+        flash('Tipo da consulta inválido', 'error')
+
+    if nome_valido == False:
+        flash('Nome inválido', 'error')
+
+    if data_valida == False:
+        flash('Data inválido', 'error')
+
+    if hora_valida == False:
+        flash('Hora inválido', 'error')
+
+    if especie_valida == False:
+        flash('Espécie inválido', 'error')
+
+    if bairro_valido == False:
+        flash('Bairro inválido', 'error')
+
+    if rua_valida == False:
+        flash('Rua inválido', 'error')
+
+    if numero_valido == False:
+        flash('N° Residencial inválido', 'error')
+
+    if complemento_valido == False:
+        flash('Complemento inválido', 'error')
+
     return redirect(url_for('marcar_consulta_page'))
 
 #-------------------------------------#
